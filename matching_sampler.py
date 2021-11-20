159af635922b5c0388d1c5e35e8fdd4ca75f2e67
Yessergire Mohamed <yessergire.mohamed@hotmail.com>
2021-11-20 21:31:28 +0200

    Updated code for upperbounds

--- matching_sampler.py
+++ matching_sampler.py
@@ -1,7 +1,14 @@
 import numpy as np
 import numpy.random as npr
+
 import networkx as nx
+from networkx.algorithms import bipartite, matching
+
+import scipy as sp
+import scipy.optimize
+
 from matching_functions import get_partitions
+
 
 def GumbelNoise(N):
     return -np.log(-np.log(npr.uniform(size=N)))
@@ -18,20 +25,58 @@ def perturb_graph(B):
     nx.set_edge_attributes(G, perturb_weights(weights),'weight')
     return G
 
-# TODO: implement code to estimate the upper bounds!
-def partial_upperbound_on_logZ(B, j, u):
-    # Ideally sum M MAPs following example in the paper
-    # 1. v_edges <- edges of v
-    # 2. remove all edges of v except (v,u)
-    # 3. run MAP
-    # 4. match_weight <- ...
-    # 5. restore v_edges to B
-    # 6. return average match_weight
-    return 0
+
+# Assumptions:
+# 1. Each call fixes previous j matches
+# 2. We consider only the next j+1 matches
+#    that is we perturb (N-j-1)x(N) entries
+def partial_upperbound_on_logZ(biadjacency, samples, j):
+    # j - current row
+    # sample containing j+1 items
+    # perturb and map M-times then average
+
+    N = biadjacency.shape[0]
+
+    # Copy original biadjacency matrix
+    weights = biadjacency.toarray()+.0
+
+    # Set samples matches
+    print(samples)
+    for i, x in enumerate(samples):
+        w = weights[i,x]
+        weights[i] = 0
+        weights[i,x] = w
+
+    # Force the matching to be perfect
+    weights[weights==0.0] = -np.inf
+
+    # For now assume M = 1
+    M = 10
+    psi = np.zeros(M)
+    for i in range(M):
+
+        # Generate iid Gumbel noise
+        noise = GumbelNoise((N-j, N))
+
+        # Ignore already sampled vertices
+        #weights[:, samples] = 0#-np.inf
+        #noise[:, samples] = 0
+
+        #print(weights)
+        # Add noise to weights
+        weights[j:] += np.exp(noise)
+
+        # MAP
+        left, right = sp.optimize.linear_sum_assignment(weights, maximize=True)
+        #print(left)
+        #print(right)
+
+        # Store the current MAP value
+        psi[i] = np.log(weights[left, right]).sum()
+
+    return psi.mean()
 
 
-
-# TODO: comment this function out, when partial_upperbound_on_logZ works
 def tmp_dist(n, j, sample, reject_p=0.5):
     D = np.zeros(n+1)
     D[-1] = reject_p
@@ -49,51 +94,66 @@ def perfect_bipartite_matching_sampler(B):
 
     # Initialize
     left_partition, right_partition = get_partitions(B)
-    reject = n = len(left_partition)
-    BIPARTITE_SAMPLE_INDECIES = []
-    map_indecies_to_labels = lambda idx: list(map(lambda id: right_partition[id], idx))
+    biadjacency = 0.0 + bipartite.biadjacency_matrix(B, row_order=left_partition, column_order=right_partition)
 
-    j = 0
-    prev_psi = 0
+    # Reject-symbol
+    n = len(left_partition)
+    reject = "R"
+    BIPARTITE_SAMPLE_INDECIES = []
+    indecies_to_labels = lambda idx: list(map(lambda id: right_partition[id], idx))
+    labels_to_indecies = {x:i for i, x in enumerate(right_partition)}
+
+    j = 1
+    # This is the 0th psi?
+    prev_psi = partial_upperbound_on_logZ(biadjacency, [], 0)
     # sample matches sequentially
-    while j < n-1:
-        v = left_partition[j]
+    while j < n:
+        v = left_partition[j-1]
         psi = {}
 
         # Probability of (v,u) given v
         # The nth index contains p(reject)
-        P = np.zeros(n+1)
+        P = np.zeros(len(B[v])+1)
 
         # We consider adjecent nodes of v in sequence.
-        # At each iteration we consider (v,u) to be
-        # a match and ignore other nodes.
+        # At each iteration we consider (v,u) to be a
+        # match and ignore other nodes adjacent to v.
         for u in B[v]:
-            if u in map_indecies_to_labels(BIPARTITE_SAMPLE_INDECIES):
+            if labels_to_indecies[u] in BIPARTITE_SAMPLE_INDECIES:
                 continue
+
             # generate upper bounds of logZ
-            psi[u] = partial_upperbound_on_logZ(biadjec, j, u)
+            tmp_sample = BIPARTITE_SAMPLE_INDECIES + [labels_to_indecies[u]]
+            psi[u] = partial_upperbound_on_logZ(biadjacency, tmp_sample, j)
 
             # calculate probability of (v,u)
-            # TODO: uncomment, when partial_upperbound_on_logZ works
-            # P[u] = np.exp(psi[u])/ np.exp(prev_psi)
-        # prev_psi = ?
+            k = labels_to_indecies[u]
+            P[k] = np.exp(psi[u])/ np.exp(prev_psi)
+        #prev_psi = np.mean(list(psi.values())) # NOT CORRECT!
+        #print(P)
+        #print(psi)
+        #print(prev_psi)
 
         # Probability of rejection
-        P[reject] = 1 - np.sum(P)
+        P[-1] = 1 - np.sum(P)
 
-        # TODO: comment, when partial_upperbound_on_logZ works
         # Tmp distribution
         # Makes sure that sampling is done without replacement
-        P = tmp_dist(n, j, BIPARTITE_SAMPLE_INDECIES, 0.1)
+        # P = tmp_dist(n, j-1, BIPARTITE_SAMPLE_INDECIES, 0.1)
 
-        sampleId = npr.choice(n+1, p=P)
+        # sample from [j] U {'R'}
+
+        sampleId = npr.choice(list(B[v])+[reject], p=P)
         if sampleId == reject:
             print("Sampler restarted!")
-            j = 0
-            prev_psi = 0
+            j = 1
+            prev_psi = partial_upperbound_on_logZ(biadjacency, [], 0)
+            biadjacency = 0.0 + bipartite.biadjacency_matrix(B, row_order=left_partition)
             BIPARTITE_SAMPLE_INDECIES = []
         else:
-            BIPARTITE_SAMPLE_INDECIES.append(sampleId)
+            BIPARTITE_SAMPLE_INDECIES.append(labels_to_indecies[sampleId])
+            #biadjacency[j-1] = 0
+            #biadjacency[j-1, sampleId] = 1
             j += 1
 
     # map ids to node labels
